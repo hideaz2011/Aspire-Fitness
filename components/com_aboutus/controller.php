76baf379ffa810f832a6d11fd22e5e2e216bbce4
Narendran jeyaraman <naren.hideaz@gmail.com>
2011-10-01 03:48:49 +0530

    About us component

---
+++ components/com_aboutus/controller.php
@@ -0,0 +1,10 @@
+<?php
+	defined( '_JEXEC' ) or die( 'Restricted access' );
+	jimport('joomla.application.component.controller');
+	class AboutusController extends JController
+	{
+		function display() {
+			parent::display();
+		}
+    }
+?>
---
+++ components/com_aboutus/views/aboutus/tmpl/default.php
@@ -0,0 +1,17 @@
+<?php defined('_JEXEC') or die('Restricted access'); 
+$app = JFactory::getApplication();
+$templateDir = JURI::base() . 'templates/' . $app->getTemplate();
+
+?>
+<div class="span-11">
+<IMG ALT="" SRC="<?php echo $templateDir; ?>/images/aboutus.jpg" />
+</div>
+
+<div class="span-12  panel last" style="margin-left:20px;">
+	<h4 class="title">About us</h4>
+		<p class="desc-para">Aspire fitness is boon for every individual whether he or she is in to Fitness or not. We are passionate about fitness and desire to help people live the fitness lifestyle, we are working hard and fast towards our mission of helping people Change their lives through fitness. We recognize that every person has their own set of goals and their own unique perspective of what a fitness centre means to them.</p>
+
+		<p class="desc-para">Some members want to shed a few extra pounds and some members simply want to get healthier. We deal with various fitness goals and it doesn't matter what size and shape you are, we will help you to become a NEW YOU, all we expect from you is commitment and Dedication towards your Goal.</p>
+
+		<p class="desc-para">Some members want to shed a few extra pounds and some members simply want to get healthier. Our gym is equipped with the best equipments that make exercising a nice experience. Our instructors at Aspire Fitness not only take care of your health and fitness but also ensure that you do exercise and eat in the right way. If you hate exercise, then come to Aspire Fitness and you will never be the same again. Come to Aspire Fitness and say goodbye to obesity.</p>
+</div>
