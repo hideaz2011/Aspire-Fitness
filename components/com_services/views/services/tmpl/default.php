5dc5ac4f16edeff5a805f90f01d7fef83170a4fa
dhanapal palanivel <dhanapal.hideaz@gmail.com>
2011-10-01 03:34:22 +0530

    changes in com_services and mycss

--- components/com_services/views/services/tmpl/default.php
+++ components/com_services/views/services/tmpl/default.php
@@ -1,17 +1,44 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
-<div class="span-12">
-<div class="span-12 last">
-Find The One That's Right For You.
-</div>
-<div class="span-12 last">
-Whether you're looking for a gym with weight training equipment, cardio equipment or other unique amenities to help you reach your fitness goals. Aspire Fitness has got you all.
-</div>
-</div>
-<div class="span-12 last">
-<div class="span-12 last">
-Club Amenities
-</div>
-<div class="span-12 last">
-We offer a type of amenities that let you relax tired muscles, sweat out toxins and relieve stress; such as steam room. Enjoy the rewards of your hard work after your intense exercise.
-</div>
-</div>
+<?php defined('_JEXEC') or die('Restricted access'); 
+$app = JFactory::getApplication();
+$templateDir = JURI::base() . 'templates/' . $app->getTemplate();
+
+?>
+
+    <div class="span-12 panel">
+          <h4 class="title">Find The One That's Right For You.</h4>
+             <p class="desc-para">Whether you're looking for a gym with weight training equipment, cardio equipment or other unique amenities to help you reach your fitness goals. Aspire Fitness has got you all.</p>
+    </div>
+    <div class="span-12 panel last">
+            <h3 class="title">Club Amenities</h3>
+        
+            <p class="desc-para">We offer a type of amenities that let you relax tired muscles, sweat out toxins and relieve stress; such as steam room. Enjoy the rewards of your hard work after your intense exercise.</p>
+        
+    </div>
+	
+    <div class="span-12">
+    	<IMG SRC="<?php echo $templateDir; ?>/images/serives.png" />
+    </div>
+    
+    <div class="span-12 panel last">
+    	<h3 class="title">Personal Training</h3>
+            <p class="desc-para">Take the guesswork out of your fitness plan and get fit faster. For a fee, with Aspire Fitness Personal Training programs, you'll be educated in the right way to eat and exercise so you can reach your goals.</p>
+	
+    	<h3 class="title">Weight Training Equipment</h3>
+	        <p class="desc-para">Aspire Fitness offers weight training equipment to work entire part of your body. Whether you're curling, pressing, benching, or squatting, you'll have a variety of ways to sculpt your physique.</p>
+		<h3 class="title">Cardio Equipment</h3>
+            <p class="desc-para">No gym routine should be without cardio exercise. We have the cardio equipment and features several types of machines to help you lose weight and improve your cardiovascular health.</p>
+    </div>
+    
+    <div class="span-12 panel"> 
+         <h3 class="title">Aspire Fitness Centre Type</h3> 
+                <p class="desc-para">Launch your fitness program at a convenient, moderately-sized club. At Aspire Fitness Active level clubs, you can get a full-body workout in minutes by using cardio equipment, strength machines, and free weights. Also we offer group exercise classes to add fun and variety to your routine.</p> 
+    </div>
+    <div class="span-12 panel last"> 
+         <h3 class="title">Group Exercise Classes</h3> 
+                <p class="desc-para">Get motivated and inspired by participating in a group exercise class. You never get bored.</p> 
+     </div>
+     
+     <div class="span-24 panel1 last"> 
+     	<h3 CLASS="title">Additional Amenities</h3> 
+ 			<p CLASS="desc-para">Whether you're practicing your layup technique or looking for senior fitness alternatives, we offer many other ways to expand your fitness horizons with a customised Exercise Prescription that only suits for your Goal.</p> 
+     </div>
